65c28190bc4960f516ca5f8cdc5520702fab1c18
Yurim <qscvh12@naver.com>
2023-02-09 13:54:22 +0900

    Update hello.py Web Crawling prac2

--- pythonprac/hello.py
+++ pythonprac/hello.py
@@ -6,26 +6,22 @@ data = requests.get('https://movie.naver.com/movie/sdb/rank/rmovie.naver?sel=pnt
 
 soup = BeautifulSoup(data.text, 'html.parser')
 
-# 전체 html 가져오는 것을 확인할 수 있다.
-# print(soup)
-
-
-a = soup.select_one('#old_content > table > tbody > tr:nth-child(3) > td.title > div > a')
-
-# 지정된 html 부분만 가져오는 것을 확인할 수 있다. (솎아냄)
-# print(a)
-
-# 속성 값 가져오기(bs4 - 꺽새)
-# print(a['href'])
-
-#old_content > table > tbody > tr:nth-child(2)
-#old_content > table > tbody > tr:nth-child(3)
-trs = soup.select('#old_content > table > tbody > tr') # tr 공통 -> tr들이 리스트로 쌓인다.
+trs = soup.select('#old_content > table > tbody > tr') # 영화 리스트 공통 부분
 for tr in trs:
-    #old_content > table > tbody > tr:nth-child(2) > td.title > div > a
+    #old_content > table > tbody > tr:nth-child(3) > td.title > div > a
     a = tr.select_one('td.title > div > a') # 영화 제목
-    print(a)
 
-    # 구분선이 None으로 표시됨을 방지
+    # 구분선 None 제거
     if a is not None:
-        print(a.text)
+        title = a.text # 영화 제목
+        # print(title)
+        
+        #old_content > table > tbody > tr:nth-child(2) > td:nth-child(1) > img
+        rank = tr.select_one('td:nth-child(1) > img')['alt'] # 랭킹 이미지[순위]
+        # print(rank)
+
+        #old_content > table > tbody > tr:nth-child(2) > td.point
+        star = tr.select_one('td.point').text # 영화 평점
+        # print(star)
+
+        print(rank, title, star)
