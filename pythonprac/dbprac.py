e79eadb6015ce66a05ba22eb86d1779494ab59f8
Yurim <qscvh12@naver.com>
2023-02-10 11:46:24 +0900

    update dprac.py and hello.py

--- pythonprac/dbprac.py
+++ pythonprac/dbprac.py
@@ -2,11 +2,27 @@ from pymongo import MongoClient
 client = MongoClient('mongodb+srv://sparta:test@cluster0.mmgalfj.mongodb.net/?retryWrites=true&w=majority') # 라이브러리 부르기
 db = client.dbsparta
 
-# 몽고DB는 딕셔너리를 넣어주면 된다 
-doc = {
-    'name':'영수',
-       'age':24
-}
 
-# 'users' 라는 명칭의 collection이다.
-db.users.insert_one(doc)
+# insert 한 개 입력
+doc = {'name':'영수','age':24} # 몽고DB는 딕셔너리를 넣어주면 된다 
+db.users.insert_one(doc) # 'users' 라는 명칭의 collection이다.
+
+
+# select 전체 조회 ( _id 값은 제외하고 출력)
+all_users = list(db.users.find({}, {'_id': False})) # {} 조건 없음, {'_id': False} '_id' 값 제거
+for i in all_users:
+    print(i)
+
+    
+# select 한 개 조회 ( _id 값은 제외하고 출력)
+user = db.users.find_one({}, {'_id': False})
+user = db.users.find_one({'name':'철수'})
+print(user)
+
+
+# update 한 개 갱신
+db.users.update_one({'name':'영수'},{'$set':{'age':19}}) # name이 영수인 것을 찾아서 age을 19로 바꿔라
+
+
+# delete 한 개 삭제
+db.users.delete_one({'name':'영수'})
--- pythonprac/hello.py
+++ pythonprac/hello.py
@@ -1,10 +1,19 @@
+# mongoDB 연결
+from pymongo import MongoClient
+client = MongoClient('mongodb+srv://sparta:test@cluster0.mmgalfj.mongodb.net/?retryWrites=true&w=majority') # 라이브러리 부르기
+db = client.dbsparta
+
+
+# 웹 크롤링에 필요한 라이브러리
 import requests
 from bs4 import BeautifulSoup
 
+
+# 웹 크롤링 준비
 headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
 data = requests.get('https://movie.naver.com/movie/sdb/rank/rmovie.naver?sel=pnt&date=20210829',headers=headers)
-
 soup = BeautifulSoup(data.text, 'html.parser')
+
 
 trs = soup.select('#old_content > table > tbody > tr') # 영화 리스트 공통 부분
 for tr in trs:
@@ -24,4 +33,7 @@ for tr in trs:
         star = tr.select_one('td.point').text # 영화 평점
         # print(star)
 
-        print(rank, title, star)
+        # print(rank, title, star)
+
+        doc = {'title':title, 'rank':rank, 'star':star}
+        db.movies.insert_one(doc)
