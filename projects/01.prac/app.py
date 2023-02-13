ae28714a0e309ddc9f6f6503d8cb36586f4d6edb
Yurim <qscvh12@naver.com>
2023-02-13 22:53:34 +0900

    Prac post method

--- projects/01.prac/app.py
+++ projects/01.prac/app.py
@@ -5,11 +5,22 @@ app = Flask(__name__)
 def home():
    return render_template('index.html')
 
+'''
+# Get 방식
 @app.route('/test', methods=['GET']) # 1. '/test'라는 창구로 GET 요청이 들어온다
 def test_get():
-   title_receive = request.args.get('title_give') # 2. 그떄 'title_give'라는 데이터가 있으면 들고 온다
-   print(title_receive)
-   return jsonify({'result':'success', 'msg': '이 요청은 GET!'}) # 백엔드에서 프론트엔드 데이터 내려주기
+   title_receive = request.args.get('title_give') # 2. 그떄 'title_give'가 들어오면 해당 데이터를 title_receive 변수에 넣는다.
+   print(title_receive) # 3. 터미널에 'None'이 찍힌다.
+   return jsonify({'result':'success', 'msg': '이 요청은 GET!'}) # 4. 백엔드에서 프론트엔드로 데이터를 내려준다.
+'''
+
+# POST 방식
+@app.route('/test', methods=['POST']) # 1. '/test'라는 창구로 POST 요청이 들어온다
+def test_post():
+   title_receive = request.form['title_give'] # 2. 그떄 'title_give'가 들어오면 해당 데이터를 title_receive 변수에 넣는다.
+   print(title_receive) # 3. 터미널에 '블랙팬서'가 찍힌다.
+   return jsonify({'result':'success', 'msg': '이 요청은 POST!'}) # 4. 백엔드에서 프론트엔드로 데이터를 내려준다.
+
 
 if __name__ == '__main__':  
    app.run('0.0.0.0',port=5000,debug=True)
